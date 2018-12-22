c426a642cbb96559e63ba6f4e21d8b0e754fe0ee
Mikael Andersson <mickepunktjohn@gmail.com>
2018-12-22 16:12:26 +0100

    Added basic security configuration. User authorization linked to application db.

---
+++ src/main/java/com/mmm/movienight/security/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.mmm.movienight.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.mmm.movienight.services.MongoUserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableConfigurationProperties
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MongoUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/newuser").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    //For custom login page
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/", true)
+                .and()
+                    .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userDetailsService);
+    }
+
+
+}
