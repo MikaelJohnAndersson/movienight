d9c26775f4de417de90e58493db3635cf2af405b
Milan Stoiljkovic <misto1977@yahoo.com>
2019-01-09 16:23:23 +0100

    Functionality for getting events from currently logged in user

---
+++ src/main/java/com/mmm/movienight/services/GoogleService.java
@@ -0,0 +1,59 @@
+package com.mmm.movienight.services;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Events;
+
+import com.mmm.movienight.models.Users;
+import com.mmm.movienight.repositories.UserRepository;
+import org.springframework.beans.BeanUtils;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class GoogleService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private final DateTime minDate = new DateTime(new Date());
+
+    public Events getEvents() {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+
+        //Getting token data and saving onto currently logged in user in db
+        Users user = userRepository.findByUsername(username);
+        String accessToken = user.getGapiDetails().getAccesstoken();
+
+        GoogleCredential credential = new GoogleCredential().setAccessToken(accessToken);
+        Calendar calendar =
+                new Calendar.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance(), credential)
+                        .setApplicationName("MOVIE_NIGHT")
+                        .build();
+        Events events = null;
+        try {
+            events = calendar.events().list("primary")
+                    .setMaxResults(50)
+                    .setTimeMin(minDate)
+                    .setOrderBy("startTime")
+                    .setSingleEvents(true)
+                    .execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return events;
+    }
+}
