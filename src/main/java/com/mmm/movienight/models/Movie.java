832141e73928df00dbd521c08a4e5c181d5a82b7
Martina J:son Gren <martinajsongren@hotmail.se>
2019-01-11 14:36:10 +0100

    Added annotation on variables in Movie-class

--- src/main/java/com/mmm/movienight/models/Movie.java
+++ src/main/java/com/mmm/movienight/models/Movie.java
@@ -4,36 +4,61 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.Transient;
 
 @Document(collection = "users")
 public class Movie {
 
     @Id
     public ObjectId id;
-
+    @JsonProperty("Title")
     private String title;
+    @JsonProperty("Year")
     private String year;
+    @JsonProperty("Rated")
     private String rated;
+    @JsonProperty("Released")
     private String released;
+    @JsonProperty("Runtime")
     private String runtime;
+    @JsonProperty("Genre")
     private String genre;
+    @JsonProperty("Director")
     private String director;
+    @JsonProperty("Writer")
     private String writer;
+    @JsonProperty("Actors")
     private String actors;
+    @JsonProperty("Plot")
     private String plot;
+    @JsonProperty("Language")
     private String language;
+    @JsonProperty("Country")
     private String country;
+    @JsonProperty("Awards")
     private String awards;
+    @JsonProperty("Poster")
     private String poster;
+    @JsonProperty("Metascore")
     private String metascore;
+    @JsonProperty("imdbRating")
     private String imdbRating;
+    @JsonProperty("imdbVotes")
     private String imdbVotes;
+    @JsonProperty("imdbID")
     private String imdbID;
+    @JsonProperty("Type")
     private String type;
+    @JsonProperty("DVD")
     private String dvd;
+    @JsonProperty("BoxOffice")
     private String boxOffice;
+    @JsonProperty("Production")
     private String production;
+    @JsonProperty("Website")
     private String website;
+    @JsonProperty("Response")
     private String response;
     @JsonIgnore
     private Object[] ratings;
