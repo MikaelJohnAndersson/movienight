c59dbb985025f212976a921eb30e3b5816099d2b
Martina J:son Gren <martinajsongren@hotmail.se>
2019-01-11 16:55:41 +0100

    Encapsulated class fields where possible

--- src/main/java/com/mmm/movienight/services/MongoUserDetailsService.java
+++ src/main/java/com/mmm/movienight/services/MongoUserDetailsService.java
@@ -14,8 +14,12 @@ import java.util.List;
 
 @Component
 public class MongoUserDetailsService implements UserDetailsService {
+<<<<<<< HEAD
 
     private final UserRepository repository;
+=======
+    final UserRepository repository;
+>>>>>>> Encapsulated class fields where possible
 
     @Autowired
     public MongoUserDetailsService(UserRepository repository) {
