68a36318e8180108921d59bc2ac19410c6141c4d
Martina J:son Gren <martinajsongren@hotmail.se>
2019-01-11 14:36:10 +0100

    Added annotation on variables in Movie-class

--- src/main/java/com/mmm/movienight/models/Movie.java
+++ src/main/java/com/mmm/movienight/models/Movie.java
@@ -1,32 +1,58 @@
 package com.mmm.movienight.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Transient;
 
 public class Movie {
+    @JsonProperty("Title")
     private String title;
+    @JsonProperty("Year")
     private String year;
+    @JsonProperty("Rated")
     private String rated;
+    @JsonProperty("Released")
     private String released;
+    @JsonProperty("Runtime")
     private String runtime;
+    @JsonProperty("Genre")
     private String genre;
+    @JsonProperty("Director")
     private String director;
+    @JsonProperty("Writer")
     private String writer;
+    @JsonProperty("Actors")
     private String actors;
+    @JsonProperty("Plot")
     private String plot;
+    @JsonProperty("Language")
     private String language;
+    @JsonProperty("Country")
     private String country;
+    @JsonProperty("Awards")
     private String awards;
+    @JsonProperty("Poster")
     private String poster;
+    @JsonProperty("Metascore")
     private String metascore;
+    @JsonProperty("imdbRating")
     private String imdbRating;
+    @JsonProperty("imdbVotes")
     private String imdbVotes;
+    @JsonProperty("imdbID")
     private String imdbID;
+    @JsonProperty("Type")
     private String type;
+    @JsonProperty("DVD")
     private String dvd;
+    @JsonProperty("BoxOffice")
     private String boxOffice;
+    @JsonProperty("Production")
     private String production;
+    @JsonProperty("Website")
     private String website;
+    @JsonProperty("Response")
     private String response;
+    @JsonProperty("Ratings")
     @Transient
     private
     Object[] ratings;
