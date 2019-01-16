ee49b8f6d95c8d1dbf1c08985d2a685145a2f58f
Martina J:son Gren <martinajsongren@hotmail.se>
2019-01-16 17:06:48 +0100

    Movie Search input gets JSON result from backend

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,3 +4,21 @@ var vm = new Vue({
         user: 'username',
     }
 });
+
+
+
+
+$('#search').keyup(function() {
+    var s = $('#search').val();
+    $.getJSON('omdb/movies', {
+        search: s
+    })
+        .done(function( data ) {
+            $.each( data["Search"], function( i, item ) {
+                console.log(item);
+            });
+        })
+    .fail(function(error) {
+        console.log(error);
+    })
+});
