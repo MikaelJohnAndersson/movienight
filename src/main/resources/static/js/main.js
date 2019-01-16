eaa60bfb4511d84a492f64db90081d6acb8dfc36
Martina J:son Gren <martinajsongren@hotmail.se>
2019-01-16 22:24:54 +0100

    Added modal to show detailed info about one movie

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -20,7 +20,7 @@ $('#search').keyup(function() {
                                 <div class="card"> 
                                 <div class="card-header">${item.Title}</div>
                          <img src="${item.Poster}" class="card-img-top" alt="${item.Title} poster">
-                         <a href="/omdb/movies/${item.Title}" class="card-footer btn btn-sm">Läs mer</a>
+                         <button class="card-footer btn btn-sm" data-toggle="modal" data-target="#movie-modal" data-movie="${item.Title}">Läs mer</button>
                         </div>
                     </div>`;
                 $(".searchResult").append(movieInfo);
@@ -30,3 +30,20 @@ $('#search').keyup(function() {
         console.log(error);
     })
 });
+
+
+
+
+$('#movie-modal').on('show.bs.modal', function (event) {
+    let button = $(event.relatedTarget) // Button that triggered the modal
+    let movieTitle = button.data('movie') // Extract info from data-* attributes
+    $.getJSON('omdb/movies/'+ movieTitle)
+        .done(function(data) {
+            let modal = $('#movie-modal')
+            modal.find('.modal-title').text(data.Title);
+            modal.find('.modal-body p').text(data.Plot);
+        })
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+
+})
