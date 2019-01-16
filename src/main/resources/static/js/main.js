c60eb9a966afeb57a06630fd98db5ce098ec9820
Martina J:son Gren <martinajsongren@hotmail.se>
2019-01-16 21:15:52 +0100

    Implemented card views for search results

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -14,8 +14,16 @@ $('#search').keyup(function() {
         search: s
     })
         .done(function( data ) {
+            $(".searchResult").empty();
             $.each( data["Search"], function( i, item ) {
-                console.log(item);
+                let movieInfo =`<div class="col-md-3">
+                                <div class="card"> 
+                                <div class="card-header">${item.Title}</div>
+                         <img src="${item.Poster}" class="card-img-top" alt="${item.Title} poster">
+                         <a href="/omdb/movies/${item.Title}" class="card-footer btn btn-sm">Läs mer</a>
+                        </div>
+                    </div>`;
+                $(".searchResult").append(movieInfo);
             });
         })
     .fail(function(error) {
